6bf1ac0eafc9505b34847dc2856ff12eec9a5499
Nik Klimenko <nikklimenko89@gmail.com>
2025-02-14 18:38:08 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import QuoteBox from './components/QuoteBox'
+
+function App() {
+	// const [count, setCount] = useState(0)
+
+	return (
+		<>
+			<QuoteBox />
+		</>
+	)
+}
+
+export default App
---
+++ src/components/QuoteBox.jsx
@@ -0,0 +1,11 @@
+import { FaQuoteLeft } from 'react-icons/fa'
+
+const QuoteBox = () => {
+	return (
+		<div>
+			<FaQuoteLeft className='text-4xl font-extrabold' />
+		</div>
+	)
+}
+
+export default QuoteBox
