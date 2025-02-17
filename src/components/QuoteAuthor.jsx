17023a55eba3f79e1cd76dddab273bd41ebf23dc
Nik Klimenko <nikklimenko89@gmail.com>
2025-02-17 17:13:24 +0200

    feat: created QuoteAuthor component

---
+++ src/components/QuoteAuthor.jsx
@@ -0,0 +1,9 @@
+function QuoteAuthor() {
+	return (
+		<div className='text-right mt-3' id='quote-author'>
+			<span className='text-orange-600'> - Author Name</span>
+		</div>
+	)
+}
+
+export default QuoteAuthor
--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,9 +1,11 @@
+import QuoteAuthor from './QuoteAuthor'
 import QuoteText from './QuoteText'
 
 const QuoteBox = () => {
 	return (
 		<div className='w-2xl min-h-48 mx-auto bg-amber-50 p-12' id='quote-box'>
 			<QuoteText />
+			<QuoteAuthor />
 		</div>
 	)
 }
